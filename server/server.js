54d99a30241bb5ca39b8716a8c1cc11fb6bb1587
AhmadImam14 <ahmad0903998@gmail.com>
2026-09-10 12:30:00 +0100

    POST error fix

--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ const adminRoutes = require('./routes/adminRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1);
+
 app.use(
   helmet({
     contentSecurityPolicy: false,
